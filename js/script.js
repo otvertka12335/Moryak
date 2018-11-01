264f95e4262910a010c4c19f94d89a6ed5202d2e
Vadim <vkravchik@gmail.com>
2018-11-01 14:54:19 +0200

    Add Feauture block Add Slider block without style and script Add Screen block

--- js/script.js
+++ js/script.js
@@ -22,14 +22,14 @@ $(window).scroll(function() {
     var scroll = $(window).scrollTop();
 
     if (scroll >= 100) {
-        $("#XYI").addClass("header-content-scrolled");
+        $("#header-content").addClass("header-content-scrolled");
         $('#fun').addClass('fun');
         $('#weather').addClass('weather');
         $('#facebookimg').attr("src", "img/facebook_black.png")
         $('#instagramimg').attr("src", "img/instagram_black.png")
         $('#twitterimg').attr("src", "img/twitter_black.png")
     } else {
-        $("#XYI").removeClass("header-content-scrolled");
+        $("#header-content").removeClass("header-content-scrolled");
         $('#fun').removeClass('fun');
         $('#weather').removeClass('weather');
         $('#facebookimg').attr("src", "img/facebook.png")
